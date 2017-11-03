e1a48f8e7a5e2f5aa50d2b989050f0a50f16e174
Siavash <siavash.sedghi@gmail.com>
2017-11-03 13:57:17 -0700

    added new Controller on PUT request to retrieve new words and store them in DB

---
+++ src/main/java/me/siavash/wotd/controllers/DBUpdate.java
@@ -0,0 +1,47 @@
+package me.siavash.wotd.controllers;
+
+import me.siavash.wotd.Response;
+import me.siavash.wotd.entities.Word;
+import me.siavash.wotd.repositories.WordRepository;
+import me.siavash.wotd.util.Parser;
+import me.siavash.wotd.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/word")
+public class DBUpdate {
+
+    private final WordRepository repository;
+
+    @Autowired
+    public DBUpdate(WordRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public @ResponseBody
+    Response insert(@RequestParam(value="date", required=false, defaultValue="today") String date){
+        HttpStatus httpStatus = Utils.validate(date);
+        if (httpStatus == HttpStatus.OK){
+            Word word = Parser.get(Utils.parseDate(date));
+            Word saved = repository.save(word);
+            if (word.equals(saved)){
+                return new Response(HttpStatus.OK);
+            } else {
+                return new Response(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            return new Response(httpStatus);
+        }
+    }
+
+
+}
