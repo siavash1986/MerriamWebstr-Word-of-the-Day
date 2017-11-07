cc0c52bf027baa05637ee482c737b4720846aa86
Siavash <siavash.sedghi@gmail.com>
2017-11-07 11:47:50 -0800

    Added findWordsByDateRange in DAO to return multiple Words in a Map<String, Word> structure. The key in the map is the word date.

--- src/main/java/me/siavash/wotd/repositories/WordRepository.java
+++ src/main/java/me/siavash/wotd/repositories/WordRepository.java
@@ -9,19 +9,26 @@ import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public interface WordRepository extends JpaRepository<Word, String> {
 
-    static CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
+    CacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
             .withCache("preConfigured",
                     CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, Word.class,
                             ResourcePoolsBuilder.heap(100))
                             .build())
             .build(true);
 
-    static Cache<String, Word> wordsCache = cacheManager.getCache("preConfigured", String.class, Word.class);
+    Cache<String, Word> wordsCache = cacheManager.getCache("preConfigured", String.class, Word.class);
 
-    public default Word findWordByDate(String date){
+    default Word findWordByDate(String date){
         if (wordsCache.containsKey(date)){
             return wordsCache.get(date);
         } else{
@@ -30,5 +37,25 @@ public interface WordRepository extends JpaRepository<Word, String> {
             return word;
         }
 
+    }
+
+    @SuppressWarnings("SpringDataRepositoryMethodReturnTypeInspection")
+    default Map<String, Word> findWordsByDateRange(String dateBegin, String dateEnd){
+        List<String> dateRange = new ArrayList<>();
+        LocalDate beginDate = LocalDate.parse(dateBegin);
+        LocalDate endDate  = LocalDate.parse(dateEnd);
+        while (beginDate.isBefore(endDate.plusDays(1))){
+            dateRange.add(beginDate.format(DateTimeFormatter.ISO_DATE));
+            beginDate = beginDate.plusDays(1);
+        }
+
+        List<Word> all = findAll(dateRange);
+        Map<String, Word> wordMap = new HashMap<>();
+        all.parallelStream().forEach(word -> wordMap.put(word.getDate(), word));
+
+        return wordMap;
+
+        // This syntax is also possible but runs much slower.
+        //return findAll(dateRange).parallelStream().collect(Collectors.toMap(Word::getDate, Function.identity()));
     }
 }
