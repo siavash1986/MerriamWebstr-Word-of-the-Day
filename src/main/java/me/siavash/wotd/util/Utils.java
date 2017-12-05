040e6b1be241d53208e65d850077cc7d99078703
Siavash <siavash.sedghi@gmail.com>
2017-12-05 10:01:10 -0800

    Adapter added to convert Word objects to FlatWord

--- src/main/java/me/siavash/wotd/util/Utils.java
+++ src/main/java/me/siavash/wotd/util/Utils.java
@@ -1,8 +1,12 @@
 package me.siavash.wotd.util;
 
+import me.siavash.wotd.entities.Word;
+import me.siavash.wotd.entities.FlatWord;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.stream.Collectors;
 
 public class Utils {
 
@@ -33,5 +37,19 @@ public class Utils {
     public static String parseDate(String date) {
         return date.equals("today") ? LocalDate.now().format(DateTimeFormatter.ISO_DATE)
                 : date;
+    }
+
+    public static FlatWord flatWordAdapter(Word w) {
+        FlatWord flatWord = new FlatWord();
+        flatWord.setDate(w.getDate());
+        flatWord.setTitle(w.getTitle() != null? w.getTitle() : "");
+        flatWord.setAttribute(w.getAttribute() != null ? w.getAttribute() : "");
+        flatWord.setSyllables(w.getSyllables() != null ? w.getSyllables() : "");
+        flatWord.setDefinition(w.getDefinition() != null? w.getDefinition().stream().collect(Collectors.joining(System.getProperty("line.separator"))) : "");
+        flatWord.setExamples(w.getExamples() != null? w.getExamples().stream().collect(Collectors.joining(System.getProperty("line.separator"))) : "");
+        flatWord.setDidYouKnow(w.getDidYouKnow() != null ? w.getDidYouKnow() : "");
+        flatWord.setPodcastUrl(w.getPodcastUrl() != null ? w.getPodcastUrl() : "");
+        flatWord.setImageUrl(w.getImageUrl() != null ? w.getImageUrl() : "");
+        return flatWord;
     }
 }
